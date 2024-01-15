3c15ae401a164a207818f8bac1fc50a70bc8c730
RajneshB <rajneshbaiju@gmail.com>
2024-01-15 20:49:03 +0530

    try everything

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Blog-preview-card_frontend_mentor/",
   plugins: [react()],
 })
